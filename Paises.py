0589c8c7d7e596b71b2dfe661df28b3455bd3170
JoseColins1OO <102337505+JoseColins1OO@users.noreply.github.com>
2026-09-08 23:17:55 -0600

    Add files via upload

---
+++ Paises.py
@@ -0,0 +1,46 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+# Guardar el script completo en un archivo .py local
+script_code = """import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+
+url = "https://www.scrapethissite.com/pages/simple/"
+response = requests.get(url)
+response.encoding = 'utf-8'
+
+
+texto = BeautifulSoup(response.text, "html.parser")
+paises = texto.find_all("div", class_="country")
+
+datos = []
+
+# 4. Iterar sobre cada tarjeta para "pescar" los datos sueltos
+for pais in paises:
+    # Extraer el título del atributo 'title' dentro de la etiqueta <a>
+    paisName = pais.find("h3", class_="country-name").text.strip()
+
+    # Extraer el precio dentro de la clase .price_color
+    paisCapital = pais.find("span", class_="country-capital").text.strip()
+
+    population = int(pais.find("span", class_="country-population").text.strip())
+
+    country_area = float(pais.find("span", class_="country-area").text.strip())
+
+    datos.append({
+        "paisName": paisName,
+        "paisCapital": paisCapital,
+        "population": population,
+        "country-area": country_area
+    })
+
+
+    # 3. Convertir a DataFrame
+df = pd.DataFrame(datos)
+df.to_csv("Paises_catalogo.csv", index=False)
+print("Scraping exitoso y archivo catalogo_libros.csv creado.")
+"""
+
+with open("scraper.py", "w", encoding="utf-8") as f:
+    f.write(script_code)
